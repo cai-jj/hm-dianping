769d6058dfd9d7aa8c656298b610ef72855350a4
cjj <jijian2077@gmail.com>
2024-01-25 17:48:48 +0800

    关注功能： 使用Set实现共同关注功能 使用feed流推模式推送最新笔记给关注的粉丝 使用ZSet按照时间戳实现滚动分页查询笔记

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -29,19 +29,13 @@ public class BlogController {
 
     @Resource
     private IBlogService blogService;
-    @Resource
-    private IUserService userService;
+
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+       return blogService.saveBlog(blog);
     }
+
 
     @PutMapping("/like/{id}")
     public Result likeBlog(@PathVariable("id") Long id) {
@@ -76,5 +70,26 @@ public class BlogController {
     @GetMapping("/likes/{id}")
     public Result queryBlogLikes(@PathVariable("id") Long id) {
         return blogService.queryBlogLikes(id);
+    }
+
+    // BlogController  根据id查询博主的探店笔记
+    @GetMapping("/of/user")
+    public Result queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
+    //实现滚动分页查询
+    //每次都查询记录最小值，和最小值重复的个数，下次携带前一次查询的最小值和重复个数，从重复个数下一个开始查询
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow( @RequestParam("lastId") Long max,
+                                     @RequestParam(value = "offset", defaultValue = "0") Integer offset) {
+        return blogService.queryBlogOfFollow(max, offset);
     }
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,25 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 /**
  * <p>
@@ -16,5 +31,68 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IUserService userService;
+
+    @Override
+    public Result follow(Long id, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "follow:" + userId;
+        if(isFollow) {
+            //关注
+            Follow follow = new Follow();
+            follow.setFollowUserId(id);
+            follow.setUserId(userId);
+            boolean isSuccess = save(follow);
+            if(isSuccess) {
+                stringRedisTemplate.opsForSet().add(key, id.toString());
+            }
+        } else {
+            //取关
+            boolean isSuccess = remove(new QueryWrapper<Follow>().
+                    eq("user_id", userId).eq("follow_user_id", id));
+            if(isSuccess) {
+                stringRedisTemplate.opsForSet().remove(key, id.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    public Result isFollow(Long followId) {
+        //select count(*) from tb_follow where user_id = ?
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followId).count();
+        return Result.ok(count > 0 ? true : false);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        String userKey = "follow:" + userId;
+        String key = "follow:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(userKey, key);
+        for (String s : intersect) {
+            System.out.println(s);
+        }
+        log.debug("共同关注");
+        if (intersect == null || intersect.isEmpty()) {
+            // 无交集
+            log.debug("无交集");
+            return Result.ok(Collections.emptyList());
+        }
+        // 3.解析id集合
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        // 4.查询用户
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(users);
+    }
+
 
 }
